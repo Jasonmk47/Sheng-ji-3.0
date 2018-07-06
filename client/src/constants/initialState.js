c2ebd9eb3809ed5e7653adbd67b6d2248cf61bdc
Jason Kim <minjaekim47@gmail.com>
2018-07-06 02:02:16 -0400

    More helper files and start filling out redux state

--- client/src/constants/initialState.js
+++ client/src/constants/initialState.js
@@ -1,8 +1,22 @@
+import suits from './enums.js';
+
 const Account = {};
 
+//These fields will have to be initialized on game load
 const Game = {
   currentGameId: -1,
-  cardsVisible: [],
+  // Cards selected will be handled in component state
+  display: {
+    cardsVisible: [],
+    cardsInHand: [],
+  },
+  boardState: {
+    trumpSuit: suits.jokers,
+    trumpNumber: -1,
+    selfPoints: 0,
+    teamPoints: 0,
+    currentPlayerId: -1,
+  },
 };
 
 export const initialState = {
--- client/src/services/cardHelpers.js
+++ client/src/services/cardHelpers.js
@@ -1,10 +1,29 @@
 import suits from '../constants/enums';
+import cardToPictureDict from '../constants/cardToPicture';
 
 export const getSuitIdFromCardId = cardId => {
+  validateCardId(cardId);
   cardId %= 54;
   if (cardId < 13) return suits.clubs;
   else if (cardId < 26) return suits.diamonds;
   else if (cardId < 39) return suits.hearts;
   else if (cardId < 52) return suits.spades;
   else return suits.jokers;
+};
+
+export const getPictureUrlFromCardId = cardId => {
+  validateCardId(cardId);
+  return cardToPictureDict[cardId % 54];
+};
+
+export const getIsTrumpFromCardId = (cardId, gameState) => {
+  validateCardId(cardId);
+  const suit = getSuitIdFromCardId(cardId);
+  if (suit === suits.jokers || suit === gameState.boardState.trumpSuit)
+    return true;
+  else return false;
+};
+
+const validateCardId = cardId => {
+  if (cardId < 0) throw new Error('CardId was negative: ' + cardId);
 };
