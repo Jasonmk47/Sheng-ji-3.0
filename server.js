4a457f0523d034b615098eaa38e0796284c791e4
Jason Kim <minjaekim47@gmail.com>
2018-08-12 11:15:24 -0700

    Add new constructor back to card.jsx, remove unnecessary packages, add postgresql connection to the server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+//Keep this at the top. sets environment
+require('dotenv').config({ path: __dirname + '/.env' });
+
 const express = require('express');
 const path = require('path');
 var graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+const { Client } = require('pg');
 
 // GraphQL schema
 var schema = buildSchema(`
@@ -39,3 +43,26 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Postgres setup
+// Hard coded fallback (put in .env file if you want to check against real db
+// and don't commit .env file)
+const db = process.env.DATABASE_URL || 'postgres://localhost:5432/mylocaldb';
+
+const client = new Client({
+  connectionString: db,
+  ssl: dev,
+});
+
+client.connect();
+
+client.query(
+  'SELECT table_schema,table_name FROM information_schema.tables;',
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      //console.log(JSON.stringify(row));
+    }
+    client.end();
+  },
+);
