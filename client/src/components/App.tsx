258170c2ae5298761b13f2d85b3df6ea4db3a49b
Jason Kim <minjaekim47@gmail.com>
2018-12-11 00:26:12 -0500

    Start redirects and routes

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,17 +1,59 @@
 import * as React from 'react';
+import { css } from 'glamor';
+import { Redirect } from 'react-router-dom';
 
-import '../styles/App.css';
+class App extends React.PureComponent<{}, IState> {
+  state = {
+    redirect: false,
+  };
 
-class App extends React.PureComponent {
+  setRedirect = () => {
+    this.setState({
+      redirect: true,
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to={'/game/'} />;
+    }
+    return null;
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Sheng Ji</h1>
+      <div className={appStyle.toString()}>
+        <header className={appHeader.toString()}>
+          <h1>Sheng Ji</h1>
         </header>
+        <div className={buttonWrapper.toString()}>
+          {this.renderRedirect()}
+          <button onClick={this.setRedirect}>Create Game</button>
+        </div>
+        <div className={buttonWrapper.toString()}>
+          {this.renderRedirect()}
+          <button onClick={this.setRedirect}>Join Game</button>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+interface IState {
+  redirect: boolean;
+}
+
+const appStyle = css({
+  textAlign: 'center',
+});
+
+const appHeader = css({
+  backgroundColor: '#222',
+  height: '150px',
+  padding: '20px',
+  color: 'white',
+});
+
+const buttonWrapper = css({});
--- client/src/components/GameBoard/GameBoard.tsx
+++ client/src/components/GameBoard/GameBoard.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
+import { css } from 'glamor';
 
 import PlayingField from './PlayingField';
 import Hand from './Hand';
+import { Redirect } from 'react-router-dom';
 
-class GameBoard extends React.PureComponent  {
+class GameBoard extends React.PureComponent<{}, {}> {
+  state = {
+    toHome: false,
+  };
+
+  setRedirect = () => {
+    this.setState({
+      toHome: true,
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.state.toHome) {
+      return <Redirect to={'/'} />;
+    }
+    return null;
+  };
+
   render() {
-
     return (
       <div className="game-board">
+        <div className={buttonWrapper.toString()}>
+          {this.renderRedirect()}
+          <button onClick={this.setRedirect}>Back to Home</button>
+        </div>
         Game Board
         <PlayingField />
         <Hand />
@@ -17,3 +39,5 @@ class GameBoard extends React.PureComponent  {
 }
 
 export default GameBoard;
+
+const buttonWrapper = css({});
