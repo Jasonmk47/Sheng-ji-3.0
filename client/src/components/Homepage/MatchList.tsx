46b546960297177ab000e847b1d6282ddd8a908c
Jason Kim <minjaekim47@gmail.com>
2019-07-17 01:43:36 -0400

    Minor cosmetic updates to matchlist

--- client/src/components/Homepage/MatchList.tsx
+++ client/src/components/Homepage/MatchList.tsx
@@ -11,7 +11,7 @@ import { GET_ALL_MATCHES } from '../../services/resolvers/queries';
 
 export const MatchList = React.memo(
   withRouter(({ history }: IProps) => {
-    const [selectedGameId, setGameId] = React.useState();
+    const [selectedMatchId, setMatchId] = React.useState();
 
     return (
       <div className={wrapperCss.toString()}>
@@ -30,26 +30,28 @@ export const MatchList = React.memo(
               if (data === undefined || data.allMatches === undefined) {
                 return null;
               }
-              // TODO Filter out non active games
-              return data.allMatches.map(match => {
-                return (
-                  <MatchListItem
-                    key={match.matchId}
-                    match={match}
-                    onClick={() => setGameId(match.currentGame.gameId)}
-                  />
-                );
-              });
+              return data.allMatches
+                .filter(m => m.isActive)
+                .map(match => {
+                  return (
+                    <MatchListItem
+                      key={match.matchId}
+                      match={match}
+                      onClick={() => setMatchId(match.matchId)}
+                      isSelected={match.matchId === selectedMatchId}
+                    />
+                  );
+                });
             }}
           </AllMatchesQuery>
         </ul>
         <div className={buttonWrapperCss.toString()}>
           <Button
             text={'Join Game'}
-            isDisabled={!!selectedGameId}
+            isDisabled={!selectedMatchId}
             onClick={() => {
               // Route to game and then request
-              selectedGameId || history.push(gameRouteBase + selectedGameId);
+              selectedMatchId && history.push(gameRouteBase + selectedMatchId);
             }}
           />
         </div>
--- client/src/components/Homepage/MatchListItem.tsx
+++ client/src/components/Homepage/MatchListItem.tsx
@@ -3,52 +3,61 @@ import { css } from 'glamor';
 import { Match } from '../../types/graphqlTypes';
 import { assertHasValue } from '../../services/Throw';
 
-export const MatchListItem = React.memo(({ match, onClick }: IProps) => {
-  const { isActive, numPlayers, matchInfos, matchId } = match;
+export const MatchListItem = React.memo(
+  ({ match, onClick, isSelected }: IProps) => {
+    const { numPlayers, matchInfos, matchId } = match;
 
-  if (!isActive) {
-    return null;
-  }
+    const sortedMatchInfos = matchInfos.sort((a, b) => a.orderId - b.orderId);
 
-  // TODO get real current user id
-  const currentUserId = '11111111-1111-1111-1111-111111111111';
+    // TODO get real current user id
+    const currentUserId = '11111111-1111-1111-1111-111111111111';
 
-  // Truncate scores if even number of players, else display all scores
-  const playerScore = assertHasValue(
-    matchInfos.find(mi => mi.user.userId === currentUserId),
-    'Tried to show match info for game without user',
-  ).userScore;
-  const otherPlayerMatchInfos = matchInfos.filter(
-    mi => mi.user.userId !== currentUserId,
-  );
-  const enemyUserMatchInfo = otherPlayerMatchInfos.find(
-    opmi => opmi.userScore !== playerScore,
-  ); // Find other team by finding different score
-  const otherPlayerScores =
-    numPlayers % 2 === 1
-      ? otherPlayerMatchInfos.map(mi => mi.userScore)
-      : [enemyUserMatchInfo ? enemyUserMatchInfo.userScore : playerScore]; // If another team wasnt found by finding a different score, it's tied
+    // Truncate scores if even number of players, else display all scores
+    const playerScore = assertHasValue(
+      sortedMatchInfos.find(mi => mi.user.userId === currentUserId),
+      'Tried to show match info for game without user',
+    ).userScore;
 
-  return (
-    <li className={listCss.toString()} onClick={onClick}>
-      <span>
-        Players{' '}
-        {matchInfos
-          .sort((a, b) => a.orderId - b.orderId)
-          .map(mi => mi.user.username)
-          .join(', ')}
-      </span>
-      <span>
-        {playerScore} to {otherPlayerScores.join(' to ')}
-      </span>
-      <span>Match Id: {matchId}</span>
-    </li>
-  );
-});
+    const hasTwoTeams = numPlayers % 2 !== 1;
+
+    const isTied = !sortedMatchInfos.some(mi => mi.userScore !== playerScore);
+
+    // If another team wasnt found by finding a different score, it's tied
+    const otherPlayersScores = !hasTwoTeams
+      ? sortedMatchInfos
+          .filter(mi => mi.user.userId === currentUserId)
+          .map(mi => mi.userScore)
+      : isTied
+      ? [playerScore]
+      : [sortedMatchInfos.find(mi => mi.userScore !== playerScore)!.userScore];
+
+    return (
+      <li
+        className={css(listItemCss, isSelected && selectedCss).toString()}
+        onClick={onClick}
+      >
+        <div>
+          <div>Match Id: {matchId}</div>
+          <span>Players: </span>
+          {matchInfos
+            .sort((a, b) => a.orderId - b.orderId)
+            .map(mi => mi.user.username)
+            .join(', ')}
+        </div>
+        <div>
+          {playerScore} to {otherPlayersScores.join(' to ')}
+        </div>
+      </li>
+    );
+  },
+);
 
 interface IProps {
   match: Match;
   onClick(): void;
+  isSelected: boolean;
 }
 
-const listCss = css({});
+const listItemCss = css({ marginTop: '5px' });
+
+const selectedCss = css({ color: 'red' });
